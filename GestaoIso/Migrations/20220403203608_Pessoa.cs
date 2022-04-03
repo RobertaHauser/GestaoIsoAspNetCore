724ca6d7b21e1589c872bf9e269000e44846f749
ROBERTA HAUSER <201907225633@alunos.estacio.br>
2022-04-03 17:41:09 -0300

    Home, link para Funcionário

---
+++ GestaoIso/Migrations/20220403203608_Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoIso.Migrations
+{
+    public partial class Pessoa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    PessoaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CpfCnpj = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rg = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rua = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneResidencial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneCelular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CriacaoResp = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CriacaoData = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RevisaoResp = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RevisaoData = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.PessoaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
