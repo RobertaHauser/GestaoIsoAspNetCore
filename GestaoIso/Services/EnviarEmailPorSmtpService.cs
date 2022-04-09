eae2db4401fc609b766b402898158e421ad4afa7
ROBERTA HAUSER <201907225633@alunos.estacio.br>
2022-04-09 11:04:46 -0300

    Areas/Identity/Pages/Adicionar/Identidade

---
+++ GestaoIso/Services/EnviarEmailPorSmtpService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace GestaoIso.Services
+{
+    public class EnviarEmailPorSmtpService : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
